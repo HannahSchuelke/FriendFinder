c923f6f440726bad851cf7386b14bd0063858353
Hannah Schuelke <hannah.r.schuelke@gmail.com>
2019-03-04 17:03:07 -0600

    linking done

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,3 +2,12 @@
 
 // A GET Route to /survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
+
+// HTML Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+app.get("/survey.html", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
